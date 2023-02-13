fc423297f98994a1e3ea9e0172c799c40ebe37e7
Dennis <dwhalen@novusnotions.com>
2023-02-13 10:59:03 -0500

    Needed to add console line

--- Program.cs
+++ Program.cs
@@ -7,6 +7,8 @@ namespace MyApp // Note: actual namespace depends on the project name.
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Fix the bug");
+
         }
     }
 }
